851772c0c9272ebc45f490889a77f686fbb8c321
slayerrr12 <eyeamarinsharma@gmail.com>
2023-06-18 16:56:59 +0530

    PATCH :integrated firebase into my project and now it uses realtime database

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,5 +1,4 @@
 import { useContext } from 'react';
-
 import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 import CartContext from '../../../store/cart-context';
@@ -7,7 +6,8 @@ import CartContext from '../../../store/cart-context';
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const price = `$${props.price.toFixed(2)}`;
+  // Check if props.price exists before using toFixed
+  const price = props.price ? `$${props.price.toFixed(2)}` : '';
 
   const addToCartHandler = amount => {
     cartCtx.addItem({
@@ -26,7 +26,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
